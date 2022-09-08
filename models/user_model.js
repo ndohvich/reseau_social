03271be651b474828e0e7235b0a36a353f106215
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2022-09-08 19:18:07 +0200

    Création du modèle utiilisateur

---
+++ models/user_model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
+const userSchema = new mongoose.Schema({
+    pseudo: {
+        type: String, 
+        required: true,
+        minLength: 3,
+        maxLength: 55,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: [isEmail],
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+        minlength: 6
+    },
+    picture: {
+        type: String,
+        default: "./uploads/profil/random-user.png"
+    },
+    bio: {
+        type: String,
+        max: 1024,
+    },
+    followers: {
+        type: [String]
+    },
+    following: {
+        type: [String]
+    },
+    likes: {
+        type: [String]
+    }
+},
+{
+    timestamps: true,
+}
+);
