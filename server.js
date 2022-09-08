c775a93f6fcba06466d5e2652149547b905fa350
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2022-09-08 20:52:03 +0200

    ajout du dossier controller routes et mise à jour du dossier modèle

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 
+//lancement du server
 const app = express();
 
+
+//routes
+app.use('/api/user', userRoutes);
+
+//mise en place du lancement du server
 app.listen(process.env.PORT, ()=> {
     console.log('Listening on port '+process.env.PORT); 
 })
