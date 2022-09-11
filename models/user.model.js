87c2fc40dfe94de1db63640df7fa3cbc96d3ed66
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2022-09-11 17:15:51 +0200

    Mise à jour du modèle utilisateur

--- models/user.model.js
+++ models/user.model.js
@@ -52,17 +52,5 @@ userSchema.pre("Enregistrer", async function(next) {
     next();
 });
 
-userSchema.statics.login = async function(email, password){
-    const user = await this.findOne({ email });
-    if(user){
-        const auth = await bcrypt.compare(password, user.password);
-        if(auth){
-            return user;
-        }
-        throw Error('Mot de passe Incorrect');
-    }
-    throw Error('Email Incorrecte');
-};
-
 const UserModel = mongoose.model("user", userSchema);
 module.exports = UserModel;
