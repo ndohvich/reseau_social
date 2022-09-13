e4e33b8be1dda219ddc9852e0ffd335360060d5c
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2022-09-13 14:24:09 +0200

    création du contrôleur de l'utilisateur

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+const userController = require('../controllers/user.controller');
 
+//auth
 router.post("/register", authController.signUp);
+
+//user display: 'block'
+router.get('/', userController.getAllUsers);
 
 module.exports = router;
